bd5f8e77a0ede618cbddb28e051ce1c21ff9e956
Sergio C <sergiocalvosos@gmail.com>
2020-12-02 18:30:56 +0100

    TodoApp TodoForm

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Todo app</h1>
+    <todo-form />
+</template>
+
+<script>
+import TodoForm from './TodoForm.vue'
+
+export default {
+  components: { TodoForm },
+   
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <form>
+      <input 
+      type="text"
+      class="form-control my-3"
+      placeholder="Ingrese tarea"
+      >
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
