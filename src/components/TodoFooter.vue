7fbab8ae47729509f9686c3da455d09dbba30b9f
Sergio C <sergiocalvosos@gmail.com>
2020-12-02 23:41:46 +0100

    total lista estatico p

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,19 @@
+<template>
+   <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span
+       role="button">
+        2 activos
+         </span> 
+      <span role="button">Eliminar activos</span>
+        </li>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,14 +4,22 @@
         v-for="todo in todos" :key="todo.id" 
         :todo="todo"
         />
+        <li v-if="todos.length === 0" class="list-group-item">
+             No hay elementos
+        </li>
+       
+        <todo-footer >
+           
+        </todo-footer>
     </ul>
 </template>
 
 <script>
 import { inject } from 'vue'
 import TodoItem from './TodoItem.vue'
+import TodoFooter from './TodoFooter.vue'
 export default {
-  components: { TodoItem },
+  components: { TodoItem, TodoFooter },
   setup(){
       const todos = inject('todos')
 
